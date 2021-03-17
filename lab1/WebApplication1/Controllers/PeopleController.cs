17e8b89925160b3e66d0019aed8da1aafb4913e2
WozniakMichal2 <mwozniak2@edu.cdv.pl>
2021-03-17 17:47:41 +0100

    lab1

---
+++ lab1/WebApplication1/Controllers/PeopleController.cs
@@ -0,0 +1,42 @@
+﻿using WebApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private readonly PeopleDb db;
+
+        public PeopleController(PeopleDb db)
+          {
+            this.db = db;
+          }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            /*     var people = new List<Person>
+             {
+                 new Person
+                 {
+                     FirstName = "Michal",
+                     LastName = "Wozniak",
+                     Id = 1
+                 }
+             }; */
+            var people = db.People.ToList();
+            return Ok(people);
+        }
+    }
+
+
+    
+}
---
+++ lab1/WebApplication1/PeopleDb.cs
@@ -0,0 +1,19 @@
+﻿using WebApplication1.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class PeopleDb : DbContext
+    {
+        public PeopleDb(DbContextOptions<PeopleDb> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}

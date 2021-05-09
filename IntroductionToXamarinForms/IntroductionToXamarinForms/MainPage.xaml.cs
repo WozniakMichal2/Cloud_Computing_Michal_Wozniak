d0036e816adf12f6776af021f3d55087acc09fe0
WozniakMichal2 <mwozniak2@edu.cdv.pl>
2021-05-09 21:38:54 +0200

    Dodaj pliki projektów.

---
+++ IntroductionToXamarinForms/IntroductionToXamarinForms/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace IntroductionToXamarinForms
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    //[DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            //InitializeComponent();
+
+            var sl = new StackLayout
+            {
+                Spacing = 15,
+                Margin = new Thickness(15, 120, 15, 15)
+            };
+
+            sl.Children.Add(new Label{ Text = "Email application" });
+
+            Content = sl;
+        }
+    }
+}
